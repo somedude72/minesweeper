258fd2c2a820a12e38825ce6d3e69337d7a8b579
Zichen <hello@withzichen.com>
2025-04-01 17:07:42 -0700

    Initial commit

---
+++ src/app/app.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <QApplication>
+
+#include "view/game.h"
+#include "model/data.h"
+
+class App : public QApplication {
+public:
+    App(int argc, char** argv);
+
+private slots:
+    void on_restart();
+    void on_reveal(const model::MineCoord& coord);
+
+private:
+    model::MineBoard m_board;
+    view::MineWindow* m_window;
+
+};
---
+++ src/model/data.cpp
@@ -0,0 +1,118 @@
+#include "model/data.h"
+
+#include <ctime>
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+
+#include <vector>
+#include <queue>
+
+namespace {
+
+    int32_t random_num(int32_t lower_range, int32_t upper_range) {
+        assert(upper_range > lower_range);
+        int32_t diff = upper_range - lower_range;
+        int32_t divisor = RAND_MAX / diff;
+        return lower_range + (std::rand() / divisor);
+    }
+
+} // internal namespace
+
+namespace model {
+
+    bool MineCoord::operator==(const MineCoord& other) const {
+        return this->row == other.row && this->col == other.col;
+    }
+
+    bool MineCoord::operator!=(const MineCoord& other) const {
+        return !(*this == other);
+    }
+
+    MineBoard::MineBoard(uint32_t row_num, uint32_t col_num) {
+        m_board.resize(row_num, std::vector<MineSquare>(col_num));
+        generate_mines();
+        count_adjacent();
+    }
+
+    void MineBoard::floodfill(const MineCoord& start) {
+        std::queue<MineCoord> queue;
+        queue.push(start);
+        while (!queue.empty()) {
+            MineCoord curr = queue.front();
+            m_board[curr.row][curr.col].is_revealed = true;
+            queue.pop();
+            const int dir_row[4] = { 0, 0, -1, 1 };
+            const int dir_col[4] = { -1, 1, 0, 0 }; 
+
+            for (int32_t i = 0; i < 4; i++) {
+                int32_t new_row = curr.row + dir_row[i];
+                int32_t new_col = curr.col + dir_col[i];
+                if (new_row < 0 || new_col < 0 || new_row >= row_size() || new_col >= col_size())
+                    continue;
+                if (m_board[new_row][new_col].is_mine || m_board[new_row][new_col].is_revealed)
+                    continue;
+                if (m_board[new_row][new_col].adjacent_mines) {
+                    m_board[new_row][new_col].is_revealed = true;
+                } else {
+                    m_board[new_row][new_col].is_revealed = true;
+                    queue.push({ new_row, new_col });
+                }
+            }
+        }
+    }
+
+    const MineSquare& MineBoard::get_square(const MineCoord& get_coord) const {
+        return m_board[get_coord.row][get_coord.col];
+    }
+
+    void MineBoard::set_square(const MineCoord& set_coord, const MineSquare& value) {
+        m_board[set_coord.row][set_coord.col] = value;
+    }
+
+    int32_t MineBoard::row_size() const {
+        return m_board.size();
+    }
+
+    int32_t MineBoard::col_size() const {
+        return m_board[0].size();
+    }
+    
+    void MineBoard::generate_mines() {
+        std::srand(std::time(nullptr));
+        int32_t max_row = row_size();
+        int32_t max_col = col_size();
+        
+        for (int32_t i = 0; i < max_row; i++) {
+            for (int32_t j = 0; j < max_col; j++) {
+                bool is_mine = random_num(0, 15) == 1;
+                m_board[i][j].is_mine = is_mine;
+                m_board[i][j].is_revealed = false;
+                m_board[i][j].adjacent_mines = 0;
+            }
+        }
+    }
+
+    void MineBoard::count_adjacent() {
+        int32_t max_row = row_size();
+        int32_t max_col = col_size();
+
+        for (int32_t i = 0; i < max_row; i++) {
+            for (int32_t j = 0; j < max_col; j++) {
+                const int dir_row[8] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+                const int dir_col[8] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+                
+                for (int32_t k = 0; k < 8; k++) {
+                    int adj_row = i + dir_row[k];
+                    int adj_col = j + dir_col[k];
+                    if (adj_row < 0 || adj_col < 0 || adj_row >= max_row || adj_col >= max_col)
+                        continue;
+                    if (m_board[adj_row][adj_col].is_mine) {
+                        m_board[i][j].adjacent_mines++;
+                    }
+                }
+            }
+        }
+    }
+
+} // namespace model
