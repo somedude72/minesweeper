af4aabf632f6656a958e9620dc6bd92ba54c81dd
somedude72 <somedude72@users.noreply.github.com>
2025-04-02 20:52:47 -0700

    Cleaned include formats

--- src/view/button.cpp
+++ src/view/button.cpp
@@ -1,12 +1,40 @@
 #include "view/button.h"
 #include "model/data.h"
 
-#include <QPushButton>
-#include <QMouseEvent>
+#include "QPushButton"
+#include "QMouseEvent"
 
 namespace view {
 
+    const char* btn_regular = R"(
+    QPushButton {
+    	border: 0px solid gray;
+    	border-right: 4px solid gray;
+        border-bottom: 4px solid gray;
+        border-top: 4px solid white;
+        border-left: 4px solid white;
+    	background-color: rgb(205, 205, 205);
+    	color: rgb(0, 0, 0);
+    }
+        
+    QPushButton:pressed {
+        border: 0px solid gray;
+    	border-right: 4px solid white;
+        border-bottom: 4px solid white;
+        border-top: 4px solid gray;
+        border-left: 4px solid gray;
+    	background-color: rgb(205, 205, 205);
+    	color: rgb(0, 0, 0);
+    }
+        
+    QPushButton:disabled {
+        border: 0.5px solid black;
+    	background-color: rgb(205, 205, 205);
+    	color: rgb(0, 0, 0);
+    })";
+
     MineButton::MineButton(const model::MineCoord& coord, QWidget* parent) : QPushButton(parent) {
+        setStyleSheet(btn_regular);
         m_coord = coord;
     }
 
