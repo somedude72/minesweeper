7af6fb805473140a785818c6705c15c7e4c62aab
Zichen <hello@withzichen.com>
2025-04-02 21:32:41 -0700

    Improved styling

--- src/view/button.cpp
+++ src/view/button.cpp
@@ -7,30 +7,30 @@
 namespace view {
 
     const char* btn_regular = R"(
-    QPushButton {
-    	border: 0px solid gray;
-    	border-right: 4px solid gray;
-        border-bottom: 4px solid gray;
-        border-top: 4px solid white;
-        border-left: 4px solid white;
-    	background-color: rgb(205, 205, 205);
-    	color: rgb(0, 0, 0);
-    }
-        
-    QPushButton:pressed {
-        border: 0px solid gray;
-    	border-right: 4px solid white;
-        border-bottom: 4px solid white;
-        border-top: 4px solid gray;
-        border-left: 4px solid gray;
-    	background-color: rgb(205, 205, 205);
-    	color: rgb(0, 0, 0);
-    }
-        
-    QPushButton:disabled {
-        border: 0.5px solid black;
-    	background-color: rgb(205, 205, 205);
-    	color: rgb(0, 0, 0);
+        QPushButton {
+            border: 0px solid gray;
+            border-right: 4px solid gray;
+            border-bottom: 4px solid gray;
+            border-top: 4px solid white;
+            border-left: 4px solid white;
+            background-color: rgb(205, 205, 205);
+            color: rgb(0, 0, 0);
+        }
+            
+        QPushButton:pressed {
+            border: 0px solid gray;
+            border-right: 4px solid white;
+            border-bottom: 4px solid white;
+            border-top: 4px solid gray;
+            border-left: 4px solid gray;
+            background-color: rgb(205, 205, 205);
+            color: rgb(0, 0, 0);
+        }
+            
+        QPushButton:disabled {
+            border: 0.5px solid black;
+            background-color: rgb(205, 205, 205);
+            color: rgb(0, 0, 0);
     })";
 
     MineButton::MineButton(const model::MineCoord& coord, QWidget* parent) : QPushButton(parent) {
