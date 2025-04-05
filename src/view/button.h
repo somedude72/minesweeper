0895580fc162628b4417275fe75ae8b178d1ab72
Zichen <connect@withzichen.com>
2025-04-04 21:57:08 -0700

    Added new features (too many to list here)

--- src/view/button.h
+++ src/view/button.h
@@ -8,27 +8,27 @@
 
 namespace view {
 
-    // Wrapper class for MineButton that includes a clicked signal
-    // that also returns the id of the base object. 
-    class MineButton : public QPushButton {
-        Q_OBJECT
-    public:
-        MineButton(const model::MineCoord& coord, QWidget* parent = nullptr);
+// Wrapper class for MineButton that includes a clicked signal
+// that also returns the id of the base object. 
+class MineButton : public QPushButton {
+    Q_OBJECT
+public:
+    MineButton(const model::MineCoord& coord, QWidget* parent = nullptr);
 
-    private slots:
-        // This slot catches the signal emitted by the base class when the button is
-        // clicked. It then emits a new clicked signal with the id of the button as its
-        // parameter. (QWidget/QPushButton mouse press event override)
-        void mouseReleaseEvent(QMouseEvent* event) override;
+private slots:
+    // This slot catches the signal emitted by the base class when the button is
+    // clicked. It then emits a new clicked signal with the id of the button as its
+    // parameter. (QWidget/QPushButton mouse press event override)
+    void mouseReleaseEvent(QMouseEvent* event) override;
 
-    signals:
-        // Signal for when the button is clicked (has id as parameter)
-        void left_clicked(const model::MineCoord& id) const;
-        void right_clicked(const model::MineCoord& id) const;
+signals:
+    // Signal for when the button is clicked (has id as parameter)
+    void left_clicked(const model::MineCoord& id) const;
+    void right_clicked(const model::MineCoord& id) const;
 
-    private:
-        model::MineCoord m_coord;
+private:
+    model::MineCoord m_coord;
 
-    };
+};
 
-}
+} // namespace view
